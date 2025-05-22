74cf4eb14552a6f5fa4aa570e6e88dea18a9aa39
Sayer Smith <137255775+sayerjsmith@users.noreply.github.com>
2025-05-22 22:34:36 +0000

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,9 @@
 window.onload = () => {
   const username = localStorage.getItem("lastLoggedInUser");
   const welcome = document.getElementById("welcome");
-
   if (username) {
     welcome.textContent = `Welcome to Kinder Academy, ${username}! 🎉`;
+  } else {
+    window.location.href = "index.html"; // no user? back to login
   }
 };
